6efa4c2c21f425ae471c9e479756de1e6d538e33
jewelercart <87797426+jewelercart@users.noreply.github.com>
2023-09-24 18:13:59 -0400

    Add files via upload

---
+++ movie_rating.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM student_records.movie_ratings
+LIMIT 0, 10
+
+-- Date: 2023-09-25 03:18
+*/
+INSERT INTO `` (`row_names`,`Movies`,`Star.wars`,`Enter.the.Dragon`,`Batman`,`SpiderMan`,`Superman`) VALUES ('1','Melissa',5,3,4,3,5);
+INSERT INTO `` (`row_names`,`Movies`,`Star.wars`,`Enter.the.Dragon`,`Batman`,`SpiderMan`,`Superman`) VALUES ('2','Rahul',4,4,5,3,4);
+INSERT INTO `` (`row_names`,`Movies`,`Star.wars`,`Enter.the.Dragon`,`Batman`,`SpiderMan`,`Superman`) VALUES ('3','Bobby',4,3,4,5,5);
+INSERT INTO `` (`row_names`,`Movies`,`Star.wars`,`Enter.the.Dragon`,`Batman`,`SpiderMan`,`Superman`) VALUES ('4','Muhammad',5,4,3,5,3);
+INSERT INTO `` (`row_names`,`Movies`,`Star.wars`,`Enter.the.Dragon`,`Batman`,`SpiderMan`,`Superman`) VALUES ('5','Dr. Liu',4,5,4,5,4);
